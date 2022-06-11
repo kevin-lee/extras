1c09efbca7703af5b4577a3d8dac98dda0f1a8d6
Kevin Lee <kevin.code@kevinlee.io>
2022-06-11 22:55:31 +1000

    Close #158 - [extras-scala-io] Add more String.color to color syntax

--- modules/extras-scala-io/shared/src/main/scala-3/extras/scala/io/syntax/ColorSyntax.scala
+++ modules/extras-scala-io/shared/src/main/scala-3/extras/scala/io/syntax/ColorSyntax.scala
@@ -12,12 +12,28 @@ trait ColorSyntax {
     def colored(color: Color): String =
       s"${color.toAnsi}$text${Color.Reset.toAnsi}"
 
-    def bold: String       = colored(Color.Bold)
-    def underlined: String = colored(Color.Underlined)
-
-    def red: String   = colored(Color.Red)
-    def green: String = colored(Color.Green)
-    def blue: String  = colored(Color.Blue)
+    def black: String         = colored(Color.black)
+    def red: String           = colored(Color.red)
+    def green: String         = colored(Color.green)
+    def yellow: String        = colored(Color.yellow)
+    def blue: String          = colored(Color.blue)
+    def magenta: String       = colored(Color.magenta)
+    def cyan: String          = colored(Color.cyan)
+    def white: String         = colored(Color.white)
+    def blackBg: String       = colored(Color.blackBg)
+    def redBg: String         = colored(Color.redBg)
+    def greenBg: String       = colored(Color.greenBg)
+    def yellowBg: String      = colored(Color.yellowBg)
+    def blueBg: String        = colored(Color.blueBg)
+    def magentaBg: String     = colored(Color.magentaBg)
+    def cyanBg: String        = colored(Color.cyanBg)
+    def whiteBg: String       = colored(Color.whiteBg)
+    def reset: String         = colored(Color.reset)
+    def bold: String          = colored(Color.bold)
+    def underlined: String    = colored(Color.underlined)
+    def blink: String         = colored(Color.blink)
+    def reversedColor: String = colored(Color.reversed)
+    def invisible: String     = colored(Color.invisible)
   }
 
 }
--- modules/extras-scala-io/shared/src/test/scala-2/extras/scala/io/syntax/ColorSyntaxSpec.scala
+++ modules/extras-scala-io/shared/src/test/scala-2/extras/scala/io/syntax/ColorSyntaxSpec.scala
@@ -12,11 +12,28 @@ import scala.io.AnsiColor
 object ColorSyntaxSpec extends Properties {
   override def tests: List[Test] = List(
     property("test text.colored(Color)", testColored),
-    property("test text.bold", testBold),
-    property("test text.underlined", testUnderlined),
-    property("test text.red", testRed),
-    property("test text.green", testGreen),
-    property("test text.blue", testBlue)
+    property("test txt.black", testBlack),
+    property("test txt.red", testRed),
+    property("test txt.green", testGreen),
+    property("test txt.yellow", testYellow),
+    property("test txt.blue", testBlue),
+    property("test txt.magenta", testMagenta),
+    property("test txt.cyan", testCyan),
+    property("test txt.white", testWhite),
+    property("test txt.blackBg", testBlackBg),
+    property("test txt.redBg", testRedBg),
+    property("test txt.greenBg", testGreenBg),
+    property("test txt.yellowBg", testYellowBg),
+    property("test txt.blueBg", testBlueBg),
+    property("test txt.magentaBg", testMagentaBg),
+    property("test txt.cyanBg", testCyanBg),
+    property("test txt.whiteBg", testWhiteBg),
+    property("test txt.reset", testReset),
+    property("test txt.bold", testBold),
+    property("test txt.underlined", testUnderlined),
+    property("test txt.blink", testBlink),
+    property("test txt.reversed", testReversed),
+    property("test txt.invisible", testInvisible)
   )
 
   import extras.scala.io.syntax.color._
@@ -34,19 +51,11 @@ object ColorSyntaxSpec extends Properties {
     actual ==== expected
   }
 
-  def testBold: Property = for {
+  def testBlack: Property = for {
     text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
   } yield {
-    val actual   = text.bold
-    val expected = colorAndReset(text, AnsiColor.BOLD)
-    actual ==== expected
-  }
-
-  def testUnderlined: Property = for {
-    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
-  } yield {
-    val actual   = text.underlined
-    val expected = colorAndReset(text, AnsiColor.UNDERLINED)
+    val actual   = text.black
+    val expected = colorAndReset(text, AnsiColor.BLACK)
     actual ==== expected
   }
 
@@ -66,11 +75,155 @@ object ColorSyntaxSpec extends Properties {
     actual ==== expected
   }
 
+  def testYellow: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.yellow
+    val expected = colorAndReset(text, AnsiColor.YELLOW)
+    actual ==== expected
+  }
+
   def testBlue: Property = for {
     text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
   } yield {
     val actual   = text.blue
     val expected = colorAndReset(text, AnsiColor.BLUE)
+    actual ==== expected
+  }
+
+  def testMagenta: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.magenta
+    val expected = colorAndReset(text, AnsiColor.MAGENTA)
+    actual ==== expected
+  }
+
+  def testCyan: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.cyan
+    val expected = colorAndReset(text, AnsiColor.CYAN)
+    actual ==== expected
+  }
+
+  def testWhite: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.white
+    val expected = colorAndReset(text, AnsiColor.WHITE)
+    actual ==== expected
+  }
+
+  def testBlackBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.blackBg
+    val expected = colorAndReset(text, AnsiColor.BLACK_B)
+    actual ==== expected
+  }
+
+  def testRedBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.redBg
+    val expected = colorAndReset(text, AnsiColor.RED_B)
+    actual ==== expected
+  }
+
+  def testGreenBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.greenBg
+    val expected = colorAndReset(text, AnsiColor.GREEN_B)
+    actual ==== expected
+  }
+
+  def testYellowBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.yellowBg
+    val expected = colorAndReset(text, AnsiColor.YELLOW_B)
+    actual ==== expected
+  }
+
+  def testBlueBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.blueBg
+    val expected = colorAndReset(text, AnsiColor.BLUE_B)
+    actual ==== expected
+  }
+
+  def testMagentaBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.magentaBg
+    val expected = colorAndReset(text, AnsiColor.MAGENTA_B)
+    actual ==== expected
+  }
+
+  def testCyanBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.cyanBg
+    val expected = colorAndReset(text, AnsiColor.CYAN_B)
+    actual ==== expected
+  }
+
+  def testWhiteBg: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.whiteBg
+    val expected = colorAndReset(text, AnsiColor.WHITE_B)
+    actual ==== expected
+  }
+
+  def testReset: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.reset
+    val expected = colorAndReset(text, AnsiColor.RESET)
+    actual ==== expected
+  }
+
+  def testBold: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.bold
+    val expected = colorAndReset(text, AnsiColor.BOLD)
+    actual ==== expected
+  }
+
+  def testUnderlined: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.underlined
+    val expected = colorAndReset(text, AnsiColor.UNDERLINED)
+    actual ==== expected
+  }
+
+  def testBlink: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.blink
+    val expected = colorAndReset(text, AnsiColor.BLINK)
+    actual ==== expected
+  }
+
+  def testReversed: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.reversedColor
+    val expected = colorAndReset(text, AnsiColor.REVERSED)
+    actual ==== expected
+  }
+
+  def testInvisible: Property = for {
+    text <- Gen.string(Gen.alphaNum, Range.linear(3, 5)).log("text")
+  } yield {
+    val actual   = text.invisible
+    val expected = colorAndReset(text, AnsiColor.INVISIBLE)
     actual ==== expected
   }
 
