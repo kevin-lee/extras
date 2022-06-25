3d55260b0733d8c9f96e299c14542a677e3376d1
Kevin Lee <kevin.code@kevinlee.io>
2022-06-25 22:42:17 +1000

    Close #172 - [extras-core] Add syntax to encode String to escaped unicode chars

---
+++ modules/extras-core/shared/src/test/scala/extras/core/syntax/StringSyntaxSpec.scala
@@ -0,0 +1,29 @@
+package extras.core.syntax
+
+import hedgehog._
+import hedgehog.runner._
+
+/** @author Kevin Lee
+  * @since 2022-06-25
+  */
+object StringSyntaxSpec extends Properties {
+  override def tests: List[Test] = List(
+    property("test String.encodeToUnicode", testEncodeToUnicode)
+  )
+
+  def testEncodeToUnicode: Property = for {
+    s <- Gen.string(Gen.unicode, Range.linear(1, 100)).log("s")
+  } yield {
+    import StringSyntax._
+    val expected = s
+      .map(_.toInt)
+      .map { c =>
+        val hex  = c.toHexString
+        val diff = 4 - hex.length
+        "\\u" + ("0" * diff) + hex
+      }
+      .mkString
+    val actual   = s.encodeToUnicode
+    actual ==== expected
+  }
+}
