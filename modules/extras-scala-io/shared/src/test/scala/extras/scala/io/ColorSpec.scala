8223de54e64b175abcf0663a466ceba498fb9ef9
Kevin Lee <kevin.code@kevinlee.io>
2022-06-26 14:16:04 +1000

    Add: more tests

--- modules/extras-scala-io/shared/src/test/scala/extras/scala/io/ColorSpec.scala
+++ modules/extras-scala-io/shared/src/test/scala/extras/scala/io/ColorSpec.scala
@@ -14,7 +14,9 @@ object ColorSpec extends Properties {
     property("test Color.color(non empty String)", testColor),
     property("test Color.color(an empty String)", testColorEmpty),
     property("test Color.colored(non empty String)", testColored),
-    property("test Color.colored(an empty String)", testColoredEmpty)
+    property("test Color.colored(an empty String)", testColoredEmpty),
+    property("test Color.show", testColorShow),
+    property("test Color.render(color)", testColorRender)
   )
 
   def testColor: Property = for {
@@ -57,6 +59,25 @@ object ColorSpec extends Properties {
     val actual   = color.colored("")
     (actual ==== expected)
       .log(s"""${actual.encodeToUnicode} should be "" (an empty String)""")
+  }
+
+  def testColorShow: Property = for {
+    colorAndAnsi <- ColorGens.genColor.log("(color, ansi)")
+    (color, _) = colorAndAnsi
+  } yield {
+    @SuppressWarnings(Array("org.wartremover.warts.ToString"))
+    val expected = color.toString
+    val actual   = color.show
+    actual ==== expected
+  }
+
+  def testColorRender: Property = for {
+    colorAndAnsi <- ColorGens.genColor.log("(color, ansi)")
+    (color, ansi) = colorAndAnsi
+  } yield {
+    val expected = color.toAnsi
+    val actual   = Color.render(color)
+    actual ==== expected
   }
 
 }
