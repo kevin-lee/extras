95efc0b85f7105bc65cead7e9f98399d9e30ed98
Kevin Lee <kevin.code@kevinlee.io>
2023-08-24 22:07:02 +1000

    StringListOps => StringSeqOps and update comment on StringSeqOps.commaAnd

--- modules/extras-string/shared/src/main/scala/extras/strings/syntax/strings.scala
+++ modules/extras-string/shared/src/main/scala/extras/strings/syntax/strings.scala
@@ -4,11 +4,35 @@ package extras.strings.syntax
   * @since 2023-08-22
   */
 trait strings {
-  implicit def stringListOps(ss: Seq[String]): strings.StringListOps = new strings.StringListOps(ss)
+  implicit def stringSeqOps(ss: Seq[String]): strings.StringSeqOps = new strings.StringSeqOps(ss)
 }
 object strings extends strings {
   @SuppressWarnings(Array("org.wartremover.warts.IterableOps"))
-  final class StringListOps(private val ss: Seq[String]) extends AnyVal {
+  final class StringSeqOps(private val ss: Seq[String]) extends AnyVal {
+
+    /** join Seq[String] with , (comma) expect for the last and the second last one which are connected with `and`.
+      * @example
+      * {{{
+      *   List.empty
+      *   // String = ""
+      *
+      *   List("")
+      *   // String = ""
+      *
+      *   List("aaa")
+      *   // String = "aaa"
+      *
+      *   List("aaa", "bbb")
+      *   // String = "aaa and bbb"
+      *
+      *   List("aaa", "bbb", "ccc")
+      *   // String = "aaa, bbb and ccc"
+      *
+      *   List("aaa", "bbb", "ccc", "ddd")
+      *   // String = "aaa, bbb, ccc and ddd"
+      *
+      * }}}
+      */
     def commaAnd: String = ss match {
       case Seq() => ""
       case Seq(s) => s
