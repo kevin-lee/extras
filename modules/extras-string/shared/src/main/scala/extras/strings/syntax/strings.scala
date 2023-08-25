b19842a9e21c92be3559a74e54761d73f9756421
Kevin Lee <kevin.code@kevinlee.io>
2023-08-25 21:56:34 +1000

    Close #419 - [extras-string] Add commaWith syntax for Seq[String]

--- modules/extras-string/shared/src/main/scala/extras/strings/syntax/strings.scala
+++ modules/extras-string/shared/src/main/scala/extras/strings/syntax/strings.scala
@@ -10,6 +10,37 @@ object strings extends strings {
   @SuppressWarnings(Array("org.wartremover.warts.IterableOps"))
   final class StringSeqOps(private val ss: Seq[String]) extends AnyVal {
 
+    /** Join Seq[String] with , (comma) expect for the last and the second last one which are connected with the given conjunction.
+      *
+      * @example
+      * {{{
+      *   List.empty[String].commaWith("blah")
+      *   // String = ""
+      *
+      *   List("").commaWith("blah")
+      *   // String = ""
+      *
+      *   List("aaa").commaWith("blah")
+      *   // String = "aaa"
+      *
+      *   List("aaa", "bbb").commaWith("blah")
+      *   // String = "aaa blah bbb"
+      *
+      *   List("aaa", "bbb", "ccc").commaWith("blah")
+      *   // String = "aaa, bbb blah ccc"
+      *
+      *   List("aaa", "bbb", "ccc", "ddd").commaWith("blah")
+      *   // String = "aaa, bbb, ccc blah ddd"
+      *
+      * }}}
+      */
+    def commaWith(conjunction: String): String = ss match {
+      case Seq() => ""
+      case Seq(s) => s
+      case Seq(s1, s2) => s"$s1 $conjunction $s2"
+      case _ => s"${ss.dropRight(1).mkString(", ")} $conjunction ${ss.last}"
+    }
+
     /** join Seq[String] with , (comma) expect for the last and the second last one which are connected with `and`.
       * @example
       * {{{
@@ -33,12 +64,7 @@ object strings extends strings {
       *
       * }}}
       */
-    def commaAnd: String = ss match {
-      case Seq() => ""
-      case Seq(s) => s
-      case Seq(s1, s2) => s"$s1 and $s2"
-      case _ => s"${ss.dropRight(1).mkString(", ")} and ${ss.last}"
-    }
+    def commaAnd: String = commaWith("and")
 
     /** join a Seq[String] with , (comma) and the last and the second last elements should be also connected with and (Oxford comma / Harvard comma).
       *
