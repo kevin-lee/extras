68a024dfd889b3ce6d3b085b0a4bff91a33b12db
Kevin Lee <kevin.code@kevinlee.io>
2023-11-05 22:48:49 +1100

    Issue #441 - [extras-string] Add extras.strings.syntax.cases to extras.strings.syntax.all

--- modules/extras-string/shared/src/main/scala/extras/strings/syntax/all.scala
+++ modules/extras-string/shared/src/main/scala/extras/strings/syntax/all.scala
@@ -3,5 +3,5 @@ package extras.strings.syntax
 /** @author Kevin Lee
   * @since 2023-10-03
   */
-trait all extends strings with numeric
+trait all extends strings with numeric with cases
 object all extends all
